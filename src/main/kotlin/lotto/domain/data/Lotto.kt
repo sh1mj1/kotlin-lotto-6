7faf3e8f8e1d7a69bfb9d092fb29571297e66ad0
sh1mj1 <7wlgns@gmail.com>
2023-12-09 14:52:35 +0900

    feat(Lotto): 로또에 대한 유효성 검증
    * 로또의 숫자는 6자리.
    * 로또에는 중복 숫자 허용 안함.
    * 각 로또 번호는 1~45 사이.

--- src/main/kotlin/lotto/domain/data/Lotto.kt
+++ src/main/kotlin/lotto/domain/data/Lotto.kt
@@ -2,8 +2,12 @@ package lotto.domain.data
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
-    }
+        require(numbers.size == 6) { "로또의 숫자는 6자리입니다." }
 
-    // TODO: 추가 기능 구현
+        require(numbers.size == numbers.toSet().size) { "로또에 중복된 숫자가 있습니다." }
+
+        numbers.forEach {
+            require(it in 1..45) { "각 로또 번호는 1~45 사이입니다." }
+        }
+    }
 }
--- src/test/kotlin/lotto/domain/data/LottoTest.kt
+++ src/test/kotlin/lotto/domain/data/LottoTest.kt
@@ -1,24 +1,33 @@
 package lotto.domain.data
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 
 class LottoTest {
     @Test
-    fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
+    fun `로또 번호의 개수가 6개가 아니면 예외가 발생한다`() {
+        val exception = assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
         }
+        assertThat(exception.message).isEqualTo("로또의 숫자는 6자리입니다.")
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
+        val exception = assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
+        assertThat(exception.message).isEqualTo("로또에 중복된 숫자가 있습니다.")
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `각 로또 번호는 1에서 45 사이이다`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+        assertThat(exception.message).isEqualTo("각 로또 번호는 1~45 사이입니다.")
+    }
+
 }
