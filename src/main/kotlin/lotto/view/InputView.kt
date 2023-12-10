537fe82e9acc5d272041a4a8287509d523e58ffc
sh1mj1 <7wlgns@gmail.com>
2023-12-10 14:10:46 +0900

    refactor(InputView)

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -4,23 +4,24 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 class InputView {
 
-    fun readPayment(input: String = readLine()): Int {
+    fun readPayment(): Int {
         println("구입금액을 입력해주세요.")
-        val payment = input.toIntOrNull()
+        val payment = readLine().toIntOrNull()
         requireNotNull(payment) { "숫자로 입력해주세요." }
         return payment
     }
 
-    fun readWinningNumbers(input: String = readLine()): List<String> {
+    fun readWinningNumbers(): List<String> {
         println("당첨 번호를 입력해 주세요.")
+        val input = readLine()
         require(input.isNotBlank()) { "당첨 번호를 제대로 입력해주세요. (공백을 입력하셨습니다.)" }
         require(onlyNumberOrDelimiter(input)) { "당첨 번호를 제대로 입력해주세요. (구분자는 , 입니다.)" }
         return input.trim().split(NUMBER_DELIMITER).map { it.trim() }
     }
 
-    fun readBonusNumber(input: String = readLine()): Int {
+    fun readBonusNumber(): Int {
         println("보너스 번호를 입력해 주세요.")
-        val number = input.toIntOrNull()
+        val number = readLine().toIntOrNull()
         requireNotNull(number) { "숫자로 입력해주세요." }
         return number
     }
